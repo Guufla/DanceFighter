5f2755f13e64487c12b31882026b19ee9799000a
Eric Nortmann <nortmanneric57@gmail.com>
2025-01-21 20:58:55 -0500

    Update HitboxCollision.cs

--- Assets/Scripts/HitboxCollision.cs
+++ Assets/Scripts/HitboxCollision.cs
@@ -23,29 +23,23 @@ public class HitboxCollision : MonoBehaviour
 
     public Slider healthbar;
     // All opponent variables
-<<<<<<< Updated upstream
-    GameObject oppositePlayer;
-    Rigidbody2D oppositeRigidBody;
 
-    float oppositeHealth;
 
     float facingX;
 
     float facingY;
-=======
+
     private GameObject oppositePlayer;
     private Rigidbody2D oppositeRigidBody;
     public float oppositeHealth = 100;  // opponents health variable
->>>>>>> Stashed changes
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< Updated upstream
-        
-=======
+
 
         player = transform.parent.gameObject; // Gets the player object (Can also get it from the game manager but i thought this was better)
 
@@ -55,80 +49,89 @@ public class HitboxCollision : MonoBehaviour
         if (player.CompareTag("Player1"))
         {
             // Takes the attack knockback variable that is set in the player attack script and creates variables for them to be used later 
-            basicAttackKnockBackX = 0; 
+            basicAttackKnockBackX = 0;
             basicAttackKnockBackY = 0;
->>>>>>> Stashed changes
-
-        player = transform.parent.gameObject;
 
 
-        // Makes sure the opposite player variable is correct
-        if(player.CompareTag("Player1")){
-            basicAttackKnockBackX = GameManager.Instance.P1AttackKnockBackX;
-
-            basicAttackKnockBackY = GameManager.Instance.P1AttackKnockBackY;
+            player = transform.parent.gameObject;
 
 
-            oppositePlayer = GameManager.Instance.player2;
+            // Makes sure the opposite player variable is correct
+            if (player.CompareTag("Player1"))
+            {
+                basicAttackKnockBackX = GameManager.Instance.P1AttackKnockBackX;
+
+                basicAttackKnockBackY = GameManager.Instance.P1AttackKnockBackY;
+
+
+                oppositePlayer = GameManager.Instance.player2;
+            }
+            else if (player.CompareTag("Player2"))
+            {
+                basicAttackKnockBackX = GameManager.Instance.P2AttackKnockBackX;
+
+                basicAttackKnockBackY = GameManager.Instance.P2AttackKnockBackY;
+
+                oppositePlayer = GameManager.Instance.player1;
+            }
+
+            oppositeRigidBody = oppositePlayer.GetComponent<Rigidbody2D>();
+
+
+            facingX = 0;
+
+            facingY = 0;
         }
-        else if(player.CompareTag("Player2")){
-            basicAttackKnockBackX = GameManager.Instance.P2AttackKnockBackX;
-
-            basicAttackKnockBackY = GameManager.Instance.P2AttackKnockBackY;
-
-            oppositePlayer = GameManager.Instance.player1;
-        }
-
-        oppositeRigidBody = oppositePlayer.GetComponent<Rigidbody2D>();
-
-
-        facingX = 0;
-
-        facingY = 0;
     }
 
-    void Update(){
-        if(player.CompareTag("Player1")){
+
+    void Update()
+
+    {
+        if (player.CompareTag("Player1"))
+        {
             basicAttackKnockBackX = GameManager.Instance.P1AttackKnockBackX;
 
             basicAttackKnockBackY = GameManager.Instance.P1AttackKnockBackY;
         }
-        else if(player.CompareTag("Player2")){
+        else if (player.CompareTag("Player2"))
+        {
             basicAttackKnockBackX = GameManager.Instance.P2AttackKnockBackX;
 
             basicAttackKnockBackY = GameManager.Instance.P2AttackKnockBackY;
         }
 
-        
+
     }
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag(oppositePlayer.tag)){
-            
+        if (other.CompareTag(oppositePlayer.tag))
+        {
+
             facingX = player.transform.localScale.x;
 
             facingY = player.transform.localScale.y;
 
             // facing accounts for which way the player is facing
 
-<<<<<<< Updated upstream
+
             // Needs to be more complex so that depending on where its hit from there will be a different knockback and stun
-            oppositeRigidBody.velocity += new Vector2(basicAttackKnockBackX * facingX,basicAttackKnockBackY * facingY );
-=======
+            oppositeRigidBody.velocity += new Vector2(basicAttackKnockBackX * facingX, basicAttackKnockBackY * facingY);
+
             // Velocity of the knockback is determined by the direction facing times the knockback variable. This is done for x and y
-            oppositeRigidBody.velocity += new Vector2(basicAttackKnockBackX * facingX,basicAttackKnockBackY * facingY);
+            oppositeRigidBody.velocity += new Vector2(basicAttackKnockBackX * facingX, basicAttackKnockBackY * facingY);
 
             // When the player is hit they lose a certain amount of health 
             oppositeHealth -= 10;
             healthbar.value = oppositeHealth; // Updates the health bar to the new health value
             if (oppositeHealth <= 0)
-            { 
+            {
                 Debug.Log("Player is dead");
             }
             // We need to adjust this later to be more dynamic
->>>>>>> Stashed changes
+
 
         }
 
